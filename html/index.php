1cb88b688f139ee109c645b8dcae52fbde77579a
Klorinmannen <root@raspberrypi>
2020-12-06 17:13:20 +0000

    index and css

--- html/index.php
+++ html/index.php
@@ -1,5 +1,15 @@
 <?php
-require_once('init.php');
+require_once('../src/init.php');
+
+session_start();
+
+$html = '<html>';
+$html .= '<head>';
+$html .= '<meta name="viewport" content="width=device-width,initial-scale=1.0">';
+$html .= '<link rel="stylesheet" href="/css/style.css">';
+$html .= '<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">';
+$html .= '</head>';
+$html .= '<body>';
+echo $html;
 
 echo 'Development site';
-
